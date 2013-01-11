935d4959c41a10dd26a55e25e552e8ed0f56746d
Kazuya NUMATA <numata@gmail.com>
2013-01-11 10:22:52 +0900

    Answer ex1.7

---
+++ ch1/exercises/1-7.scm
@@ -0,0 +1,5 @@
+;; for small number, the predetermined tolerance becomes the large difference relatively.
+
+;; for very large number, the predetermined tolerance is too small and it needs too much comparison times.
+
+
