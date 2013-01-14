96725464f883e2922482547664662145579cdfe4
Kazuya NUMATA <numata@gmail.com>
2013-01-14 16:20:52 +0900

    Implement ch1.2.1's examples

---
+++ ch1/examples/1-2-1.scm
@@ -0,0 +1,20 @@
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+(factorial 3)
+(factorial 5)
+(factorial -5)
+
+
+(define (factorial n)
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+		 (+ counter 1)
+		 max-count)))
+
+(factorial 5)
