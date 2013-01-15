e59b44c7d9e5cbdc44c53cdfec5e2040e3daae5f
Kazuya NUMATA <numata@gmail.com>
2013-01-15 11:45:21 +0900

    Implement ch1.2.2's examples

---
+++ ch1/examples/1-2-2.scm
@@ -0,0 +1,52 @@
+(define (fib n)
+  (cond ((= n 0) 0)
+	((= n 1) 1)
+	(else (+ (fib (- n 1))
+		 (fib (- n 2))))))
+
+(fib 3)
+(fib 4)
+(fib 5)
+(fib 6)
+(fib 30)
+;; iterative process
+(define (fib n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
+(fib 5)
+(fib 30)
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+	((or (< amount 0) (= kinds-of-coins 0)) 0)
+	(else (+ (cc amount
+		     (- kinds-of-coins 1))
+		 (cc (- amount
+			(first-denomination kinds-of-coins))
+		     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+	((= kinds-of-coins 2) 5)
+	((= kinds-of-coins 3) 10)
+	((= kinds-of-coins 4) 25)
+	((= kinds-of-coins 5) 50)))
+
+(count-change 100)
+(count-change 1)
+(count-change 2)
+(count-change 3)
+(count-change 4)
+(count-change 5)
+(count-change 6)
+(count-change 7)
+(count-change 10)
+
