bf9ef5b261b12771f522fde41a52e0ef9ea7ae37
Kazuya Numata <numata@gmail.com>
2013-01-24 21:55:15 +0900

    Implement ch1.3.2's examples

--- ch1/examples/1-3-2.scm
+++ ch1/examples/1-3-2.scm
@@ -32,5 +32,47 @@
 ;; Using `let' to create local variables
 ;;
 
-;; to be continued
+;; using define
+(define (f x y)
+  (define (f-helper a b)
+    (+ (* x (square a))
+       (* y b)
+       (* a b)))
+  (f-helper (+ 1 (* x y))
+	    (- 1 y)))
 
+;; using lambda
+(define (f x y)
+  ((lambda (a b)
+     (+ (* x (square a))
+	(* y b)
+	(* a b)))
+   (+ 1 (* x y))
+   (- 1 y)))
+
+;; using let
+(define (f x y)
+  (let ((a (+ 1 (* x y)))
+	(b (-1 y)))
+    (+ (* x (square a))
+       (* y b)
+       (* a b))))
+
+(let ((x 5))
+  (+ (let ((x 3))
+       (+ x (* x 10)))
+     x))
+
+(let ((x 2))
+  (let ((x 3)
+	(y (+ x 2)))
+    (* x y)))
+
+;; internal definitions (not prefered)
+(define (f x y)
+  (define a (+ 1 (* x y)))
+  (define b (- 1 y))
+  (+ (* x (square a))
+     (* y b)
+     (* a b)))
+(f 2 3)
