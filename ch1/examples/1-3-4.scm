33fcceb112593160502427e36e86015e0fc7762a
Kazuya NUMATA <numata@gmail.com>
2013-01-28 10:43:52 +0900

    Implemnet ch1.3.4's examples (in progress)

---
+++ ch1/examples/1-3-4.scm
@@ -0,0 +1,46 @@
+(define (average x y)
+  (/ (+ x y) 2))
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+((average-damp square) 10)
+
+
+;; definition of fixed-point
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+	  next
+	  (try next))))
+  (try first-guess))
+
+;; redefine sqrt whithc fixed-point and average-damp
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+	       1.0))
+
+(sqrt 3)
+(sqrt 5)
+
+;; redefine cube-root
+;; y^3 = x
+;; y = x / y^2
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (* y y))))
+	       1.0))
+
+(cube-root 27)
+(cube-root 8)
+(cube-root 10)
+(cube-root 1)
+
+
+;; Newton's method
+
+;; to be continued
