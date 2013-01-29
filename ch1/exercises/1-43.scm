31801a3c0bf43e69aa7d5623ae1792bf6aeb9650
Kazuya NUMATA <numata@gmail.com>
2013-01-29 14:25:07 +0900

    Answer ex1.43

---
+++ ch1/exercises/1-43.scm
@@ -0,0 +1,12 @@
+(define (compose f g)
+  (lambda (x)
+    (f (g x))))
+
+(define (repeated f n)
+  (if (< n 1)
+      identity
+      (compose f (repeated f (- n 1)))))
+
+((repeated square 2) 5)
+
+
