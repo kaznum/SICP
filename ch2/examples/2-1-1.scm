4e5826b947818ac7b387df4155ce712c86738e5a
Kazuya Numata <numata@gmail.com>
2013-01-31 00:35:30 +0900

    Implement ch2.1.1's examples (in progress)

--- ch2/examples/2-1-1.scm
+++ ch2/examples/2-1-1.scm
@@ -20,4 +20,20 @@
 (define (equal-rat? x y)
   (= (* (numer x) (denom y))
      (* (denom x) (numer y))))
-	       
+
+
+;; Pair
+(define x (cons 1 2))
+(car x)
+(cdr x)
+
+(define x (cons 1 2))
+(define y (cons 3 4))
+(define z (cons x y))
+
+(car (car z))
+(car (cdr z))
+
+;; Representing rational numbers
+
+;; to be continued
