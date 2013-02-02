54f945af29c6af0a997b20774090f32650a6c5ec
Kazuya NUMATA <numata@gmail.com>
2013-02-02 10:15:11 +0900

    Answer ex2.8

---
+++ ch2/exercises/2-8.scm
@@ -0,0 +1,48 @@
+;; preparation
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+		 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+	(p2 (* (lower-bound x) (upper-bound y)))
+	(p3 (* (upper-bound x) (lower-bound y)))
+	(p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+		   (max p1 p2 p3 p4))))
+
+
+(define (div-interval x y)
+  (mul-interval x (make-interval (/ 1.0 (upper-bound y))
+				 (/ 1.0 (lower-bound y)))))
+
+(define (make-interval a b) (cons a b))
+
+(define (upper-bound x) (max (car x) (cdr x)))
+(define (lower-bound x) (min (car x) (cdr x)))
+
+;;
+;; define sub-interval
+;;
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+		 (- (upper-bound x) (lower-bound y))))
+
+
+;; TEST
+
+(define (display-interval x)
+  (newline)
+  (display "(")
+  (display (lower-bound x))
+  (display ",")
+  (display (upper-bound x))
+  (display ")"))
+
+(define x (make-interval 3 5))
+(define y (make-interval 8 2))
+(display-interval x)
+(display-interval y)
+(display-interval (add-interval x y))
+(display-interval (sub-interval x y))
+
