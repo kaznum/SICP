c8ae427e505f7b076eff92e3c1100a24b627d6ec
Kazuya Numata <numata@gmail.com>
2013-02-03 12:02:16 +0900

    Implement ch2.1.4's examples

--- ch2/examples/2-1-4.scm
+++ ch2/examples/2-1-4.scm
@@ -16,3 +16,11 @@
 				 (/ 1.0 (lower-bound y)))))
 
 
+(define (make-center-width c w)
+  (make-interval (-c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
