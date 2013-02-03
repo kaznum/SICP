a7d3f7bc5bd1724aaa77a1cb0456887254ac1d17
Kazuya NUMATA <numata@gmail.com>
2013-02-04 01:34:12 +0900

    Implement ch2.2.1's examples (in progress)

--- ch2/examples/2-2-1.scm
+++ ch2/examples/2-2-1.scm
@@ -5,3 +5,23 @@
 (define one-through-four (list 1 2 3 4))
 one-through-four
 
+(car one-through-four)
+(cdr one-through-four)
+(car (cdr one-through-four)) ;; 2
+(cdr (cdr one-through-four)) ;; (3 4)
+(cdr (cdr (cdr one-through-four)))  ;; (4)
+
+(car (cdr (cdr (cdr one-through-four)))) ;; 4
+(cdr (cdr (cdr (cdr one-through-four)))) ;; ()
+
+(cdr (cdr (cdr (cdr (cdr one-through-four))))) ;; error
+(car (cdr (cdr (cdr (cdr one-through-four))))) ;; error
+
+(cons 10 one-through-four)
+(cons 5 one-through-four)
+
+;;
+;; List Operations
+;;
+
+;; to be continued
