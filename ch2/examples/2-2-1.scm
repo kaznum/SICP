2e7d8e1929ae879a627239b7cad1010d0db867b7
Kazuya NUMATA <numata@gmail.com>
2013-02-04 18:28:01 +0900

    Implement ch2.2.1's examples

--- ch2/examples/2-2-1.scm
+++ ch2/examples/2-2-1.scm
@@ -60,5 +60,29 @@ one-through-four
 
 ;; Mapping over lists
 
-;; to be continued
+(define nil '())
+(define (scale-list items factor)
+  (if (null? items)
+      nil
+      (cons (* (car items) factor)
+	    (scale-list (cdr items) factor))))
+
+(scale-list (list 1 2 3 4 5) 10)
+
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+	    (map proc (cdr items)))))
+
+(map abs (list -10 2.5 -11.6 17))
+
+(map (lambda (x) (* x x)) (list 1 2 3 4))
+
+
+;; redefine scale-list with map
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor)) items))
+(scale-list (list 1 2 3 4 5) 10)
 
