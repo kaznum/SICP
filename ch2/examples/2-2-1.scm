a4defb475765ff6a6a9d8248afce05b938ae8616
Kazuya NUMATA <numata@gmail.com>
2013-02-04 16:25:56 +0900

    Implement ch2.2.1's examples (in progress)

--- ch2/examples/2-2-1.scm
+++ ch2/examples/2-2-1.scm
@@ -24,4 +24,37 @@ one-through-four
 ;; List Operations
 ;;
 
-;; to be continued
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define squares (list 1 4 9 16 25))
+
+(list-ref squares 3)
+
+;; recursive length
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
+
+(define odds (list 1 3 5 7))
+(length odds)
+
+;; iterative length
+(define (length items)
+  (define (length-iter a count)
+    (if (null? a)
+	count
+	(length-iter (cdr a) (+ 1 count))))
+  (length-iter items 0))
+
+(define odds (list 1 3 5 7))
+(length odds)
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+
