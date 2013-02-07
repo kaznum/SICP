27ce869183b5108c5bdea0f4e68cbfc452a8f009
Kazuya NUMATA <numata@gmail.com>
2013-02-07 12:38:13 +0900

    Implement ch2.2.2's examples

--- ch2/examples/2-2-2.scm
+++ ch2/examples/2-2-2.scm
@@ -18,3 +18,22 @@
 ;; 8
 
 
+;;
+;; Mapping over trees
+;;
+(define (scale-tree tree factor)
+  (cond ((null? tree) '())
+	((not (pair? tree)) (* tree factor))
+	(else (cons (scale-tree (car tree) factor)
+		    (scale-tree (cdr tree) factor)))))
+
+
+(scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10)
+
+(define (scale-tree tree factor)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	     (scale-tree sub-tree factor)
+	     (* sub-tree factor)))
+       tree))
+
