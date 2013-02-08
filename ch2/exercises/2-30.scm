401dc96a928ab66e16a7477d033cccc2675c27bd
Kazuya NUMATA <numata@gmail.com>
2013-02-08 16:46:45 +0900

    Answer ex2.30

---
+++ ch2/exercises/2-30.scm
@@ -0,0 +1,24 @@
+;; without higher-order procedures
+(define (square-tree tree)
+  (cond ((null? tree) '())
+	((not (pair? tree)) (square tree))
+	(else (cons (square-tree (car tree)) (square-tree (cdr tree))))))
+
+;; TEST
+(square-tree (list 1
+		   (list 2 (list 3 4) 5)
+		   (list 6 7)))
+
+
+;; using map
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	     (square-tree sub-tree)
+	     (square sub-tree)))
+       tree))
+
+;; TEST
+(square-tree (list 1
+		   (list 2 (list 3 4) 5)
+		   (list 6 7)))
