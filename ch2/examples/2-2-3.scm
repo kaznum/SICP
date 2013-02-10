724127820f9b07c14ef1ca006b504ceb14bcf32c
Kazuya Numata <numata@gmail.com>
2013-02-10 22:08:56 +0900

    Implement ch2.2.3's examples

--- ch2/examples/2-2-3.scm
+++ ch2/examples/2-2-3.scm
@@ -98,3 +98,51 @@
 	      (map salary
 		   (filter programer? records))))
 
+
+;;
+;; Nested Mappings
+;;
+
+;; (accumulate append
+;; 	    '()
+;; 	    (map (lambda (i)
+;; 		   (map (lambda (j) (list i j))
+;; 			(enumerate-interval 1 (- i 1))))
+;; 		 (enumerate-interval 1 n)))
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+	       (flatmap
+		(lambda (i)
+		  (map (lambda (j) (list i j))
+		       (enumerate-interval 1 (- i 1))))
+		(enumerate-interval 1 n)))))
+
+(define (permutations s)
+  (if (null? s)
+      (list '())
+      (flatmap (lambda (x)
+		 (map (lambda (p) (cons x p))
+		      (permutations (remove x s))))
+	       s)))
+
+;; support procedure for permutations
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+	  sequence))
+;; TEST
+(remove 3 (list 1 2 3 4 5))
+(permutations '(1 2 3))
+(permutations '(1 2 3 4 5))
+
+
