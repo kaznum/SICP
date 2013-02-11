21b145ab673541ba9fe348c832dd86956aaa5a2a
Kazuya Numata <numata@gmail.com>
2013-02-11 21:54:43 +0900

    Add missing `filter' definition in ex2.40

--- ch2/exercises/2-40.scm
+++ ch2/exercises/2-40.scm
@@ -1,4 +1,11 @@
 ;; Given
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+	((predicate (car sequence))
+	 (cons (car sequence)
+	       (filter predicate (cdr sequence))))
+	(else (filter predicate (cdr sequence)))))
+
 (define accumulate fold-right)
 (define (enumerate-interval low high)
   (if (> low high)
