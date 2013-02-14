7a33c788770f04529b438ee8c64c41df0345e3c4
Kazuya NUMATA <numata@gmail.com>
2013-02-14 19:20:03 +0900

    Answer ex2.46

---
+++ ch2/exercises/2-46.scm
@@ -0,0 +1,27 @@
+(define (make-vect x y) (cons x y))
+(define (xcor-vect v) (car v))
+(define (ycor-vect v) (cdr v))
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+	     (+ (ycor-vect v1) (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+	     (- (ycor-vect v1) (ycor-vect v2))))
+
+(define (scale-vect s v)
+  (make-vect (* s (xcor-vect v)) (* s (ycor-vect v))))
+
+
+;; TEST
+(define v1 (make-vect 3 5))
+(xcor-vect v1)
+(ycor-vect v1)
+
+(define v2 (make-vect 4 1))
+
+(add-vect v1 v2)
+(sub-vect v1 v2)
+(scale-vect 5 v1)
+
