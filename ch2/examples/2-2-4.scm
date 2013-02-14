b0f857ca5e23a429b84bb19e6748c11e7ffb5371
Kazuya NUMATA <numata@gmail.com>
2013-02-14 19:19:51 +0900

    Implement ch2.2.4's examples (in progress)

--- ch2/examples/2-2-4.scm
+++ ch2/examples/2-2-4.scm
@@ -35,5 +35,39 @@
 
 ;; Higher-order operations
 
-;; to be continued
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+	  (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
 
+(define (flipped-pairs painter)
+  (let ((compine4 (square-of-four identity flip-vert
+				  identity flip-vert)))
+    (combine4 painter)))
+
+
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-horiz identity
+				  rotate180 flip-vert)))
+    (combine4 (corner-split painter n))))
+
+
+;; Frames
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v)
+			   (edge1-frame frame))
+	       (scale-vect (ycor-vect v)
+			   (edge2-frame frame))))))
+
+((frame-coord-map a-frame) (make-vect 0 0))
+;; is the same vector as
+(origin-frame a-frame)
+
+;; Painters
+
+;; to be continued
