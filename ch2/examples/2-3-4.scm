d8e25118bb21143f99fe921c7a1ccea049580b33
Kazuya Numata <numata@gmail.com>
2013-02-27 23:33:53 +0900

    Implement ch2.3.4's examples

--- ch2/examples/2-3-4.scm
+++ ch2/examples/2-3-4.scm
@@ -27,3 +27,37 @@
       (list (weight-leaf tree))
       (cadddr tree)))
 
+;; The decoding procedure
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+	'()
+	(let ((next-branch
+	       (choose-branch (car bits) current-branch)))
+	  (if (leaf? next-branch)
+	      (cons (symbol-leaf next-branch) (decode-1 (cdr bits) tree))
+	      (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+	((= bit 1) (right-branch branch))
+	(else (error "bad bit -- CHOOSE-BRANCH" bit))))
+
+;; Sets of weighted elements
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+	((< (weight x) (weight (car set))) (cons x set))
+	(else (cons (car set)
+		    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+	(adjoin-set (make-leaf (car pair)
+			       (cadr pair))
+		    (make-leaf-set (cdr pairs))))))
+
