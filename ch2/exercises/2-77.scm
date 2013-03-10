1858516fbbfc5c5fce4858994c31ce3745d6542d
Kazuya Numata <numata@gmail.com>
2013-03-10 22:37:21 +0900

    Answer ex2.77

---
+++ ch2/exercises/2-77.scm
@@ -0,0 +1,65 @@
+;; Given in the chapter.
+(define (install-complex-package)
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+                         (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+                         (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                       (- (angle z1) (angle z2))))
+
+  (define (tag z) (attach-tag 'complex z))
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  ;; Given in this exercise (ex2.77)
+  (put 'real-part '(complex) real-part)
+  (put 'imag-part '(complex) imag-part)
+  (put 'magnitude '(complex) magnitude)
+  (put 'angle '(complex) angle)
+  'done)
+
+(define (make-complex-from-real-imag x y)
+  ((get 'make-from-real-imag 'complex) x y))
+    
+(define (make-complex-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'complex) r a))
+
+;; Answer
+(magnitude z)  ;; where z is 'complex and magnitude is generic for complex
+(apply-generic 'magnitude z)
+(apply-generic 'magnitude '(complex rectangular 3 4))
+((get 'magnitude 'complex) '(rectangular 3 4))
+(magnitude '(rectangular 3 4)) ;; this magnitude is for generic for rectangle
+(apply-generic 'magnitude  '(rectangular 3 4))
+((get 'magnitude 'rectangular) '(3 4))
+(magnitude '(3 4)) ;; this magnitude is explicit for rectangle expression
+
+;; apply-generic is invoked twice.
+;; the first is for complex and the second is for rectangle expression.
+
+
+
+
+
+
+
