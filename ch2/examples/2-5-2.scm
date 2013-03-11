64fad3b44776a613a3de66d6d259aff9ab9e75b1
Kazuya NUMATA <numata@gmail.com>
2013-03-11 18:36:57 +0900

    Implement ch2.5.2's examples

---
+++ ch2/examples/2-5-2.scm
@@ -0,0 +1,14 @@
+(define (install-complex-package)
+  ;; install the following in the table using tag '(complex scheme-number)
+  (define (add-complex-to-schemenum z x)
+    (make-from-real-img (+ (real-part z) x)
+			(imag-part z)))
+
+  (put 'add '(complex scheme-number)
+       (lambda (z x) (tag (add-complex-to-schemenum z x))))
+  'done)
+
+;; Coercion
+
+;; to be continued
+
