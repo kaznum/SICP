7d97fb4d473999b36aebce0e3305ae21447df0e3
Kazuya NUMATA <numata@gmail.com>
2013-03-21 17:20:09 +0900

    Implement ch2.5.3's examples (in progress)

--- ch2/examples/2-5-3.scm
+++ ch2/examples/2-5-3.scm
@@ -1,3 +1,4 @@
+;; Arithmetic on polynominals
 (define (variable? x) (symbol? x))
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
@@ -53,4 +54,43 @@
   (put 'make 'polynomial
        (lambda (var terms) (tag (make-poly var terms))))
   'done)
+
+(define (add-terms L1 L2)
+  (cond ((empty-termlist? L1) L2)
+	((empty-termlist? L2) L1)
+	(else
+	 (let ((t1 (first-term L1)) (t2 (first-term L2)))
+	   (cond ((> (order t1) (order t2))
+		  (adjoin-term
+		   t1 (add-terms (rest-terms L1) L2)))
+		 ((< (order t1) (order t2))
+		  (adjoin-term
+		   t2 (add-terms L1 (rest-terms L2))))
+		 (else
+		  (adjoin-term
+		   (make-term (order t1)
+			      (add (coeff t1) (coeff t2)))
+		   (add-terms (rest-terms L1)
+			      (rest-terms L2)))))))))
+
+
+(define (mul-terms L1 L2)
+  (if (empty-termlist? L1)
+      (the-empty-termlist)
+      (add-terms (mul-term-by-all-terms (first-term L1) L2)
+		 (mul-terms (rest-terms L1) L2))))
+
+(define (mul-term-by-all-terms t1 L)
+  (if (empty-termlist? L)
+      (the-empty-termlist)
+      (let ((t2 (first-term L)))
+	(adjoin-term
+	 (make-term (+ (order t1) (order t2))
+		    (mul (coeff t1) (coeff t2)))
+	 (mul-term-by-all-terms t1 (rest-terms L))))))
+
+
+;; Representing term lists
+
+;; To be continued
 
