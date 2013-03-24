cfa0b2ba4f69b48a840a21e4ab90e78037d25aa2
Kazuya NUMATA <numata@gmail.com>
2013-03-24 22:06:43 +0900

    Answer ex2.91

---
+++ ch2/exercises/2-91.scm
@@ -0,0 +1,19 @@
+(define (div-terms L1 L2)
+  (if (empty-termlist? L1)
+      (list (the-empty-termlist) (the-empty-termlist))
+      (let ((t1 (first-term L1))
+	    (t2 (first-term L2)))
+	(if (> (order t2) (order t1))
+	    (list (the-empty-termlist) L1)
+	    (let ((new-c (div (coeff t1) (coeff t2)))
+		  (new-o (- (order t1) (order t2))))
+	      (let ((rest-of-result
+		     (div-terms
+		      (add-terms L1 (negate (mul-terms L2 (make-term new-o new-c)))) L2)))
+		(list (adjoin-term (make-term new-o new-c) (car rest-of-result))
+		      (cadr rest-of-result L2))))))))
+
+(define (div-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1) (div-terms (term-list p1) (term-list p2)))
+      (error "not the same variables" (list p1 p2))))
