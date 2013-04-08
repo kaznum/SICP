7212dc275de7af13697c1372542c6e31305dc5c2
Kazuya NUMATA <numata@gmail.com>
2013-04-08 18:11:30 +0900

    Fix a syntax error in ex2.91

--- ch2/exercises/2-91.scm
+++ ch2/exercises/2-91.scm
@@ -11,7 +11,7 @@
 		     (div-terms
 		      (add-terms L1 (negate (mul-terms L2 (make-term new-o new-c)))) L2)))
 		(list (adjoin-term (make-term new-o new-c) (car rest-of-result))
-		      (cadr rest-of-result L2))))))))
+		      (cadr rest-of-result))))))))
 
 (define (div-poly p1 p2)
   (if (same-variable? (variable p1) (variable p2))
