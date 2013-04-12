68e2fd2b49452e6e68a330c31ac5fb66abf8cb0f
Kazuya NUMATA <numata@gmail.com>
2013-04-12 15:47:12 +0900

    Implement ch3.1.1's examples

--- ch3/examples/3-1-1.scm
+++ ch3/examples/3-1-1.scm
@@ -17,4 +17,41 @@
 		 balance)
 	  "Insufficient funds"))))
 
-;; to be continued
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+	(begin (set! balance (- balance amount))
+	       balance)
+	"Insufficient funds")))
+
+(define W1 (make-withdraw 100))
+(define W2 (make-withdraw 100))
+(W1 50)
+(W2 70)
+(W2 40)
+(W1 40)
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+	(begin (set! balance (- balance amount))
+	       balance)
+	"Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+	  ((eq? m 'deposit) deposit)
+	  (else (error "Unknown request -- MAKE-ACCOUNT"
+		       m))))
+  dispatch)
+
+(define acc (make-account 100))
+((acc 'withdraw) 50)
+((acc 'withdraw) 60)
+((acc 'deposit) 40)
+((acc 'withdraw) 60)
+
+(define acc2 (make-account 100))
+
