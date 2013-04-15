08c006cc3e493092d96304d65474fa54d96bafd6
Kazuya Numata <numata@gmail.com>
2013-04-15 23:15:56 +0900

    Implement ch3.1.3's examples

--- ch3/examples/3-1-3.scm
+++ ch3/examples/3-1-3.scm
@@ -47,4 +47,22 @@
 
 ;; Pitfalls of Imperative programming
 
-;; to be continued
+(define (factorial n)
+  (define (iter product counter)
+    (if (> counter n)
+	product
+	(iter (* counter product)
+	      (+ counter 1))))
+  (iter 1 1))
+
+(define (factorial n)
+  (let ((product 1)
+	(counter 1))
+    (define (iter)
+      (if (> counter n)
+	  product
+	  (begin (set! product (* counter product))
+		 (set! counter (+ counter 1))
+		 (iter))))
+    (iter)))
+
