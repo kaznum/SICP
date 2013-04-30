6500560e18653298ac962ad03a3164fd3ac9cc9d
Kazuya NUMATA <numata@gmail.com>
2013-05-01 08:19:09 +0900

    Implement ch3.2.3's examples

---
+++ ch3/examples/3-2-3.scm
@@ -0,0 +1,13 @@
+(define  (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+	(begin (set! balance (- balance amount))
+	       balance)
+	"Insufficient funds")))
+
+(define W1 (make-withdraw 100))
+
+(W1 50)
+
+(define W2 (make-withdraw 100))
+
