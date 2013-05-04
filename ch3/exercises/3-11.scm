d57f0f3f710893dd4e3e0d5fa4f251163fe627d9
Kazuya NUMATA <numata@gmail.com>
2013-05-04 20:10:54 +0900

    Answer ex3.11

--- ch3/exercises/3-11.scm
+++ ch3/exercises/3-11.scm
@@ -64,3 +64,101 @@
 ;; Next, after deposit 40
 ((acc 'deposit) 40)
 
+           +---------------------------------------------------------------+
+ global -->| make-account:                                                 |
+ env       | acc: ---|------------------+                                  |
+           +---------|------------------|----------------------------------+
+                     V       ^          V        	                 ^
+                 .---.---.   |      .---.---.    	                 |
+      +----------+-O | O-+---+    +-+-O | O-+---+ 	      +----------+------------------+
+      |          `---^---'        | `---^---'   |  	E1 -> | balance: 90                 |
+      V                           V             |             | withdraw: -----+            |
+ parameters: balance           parameters: m	+------------>| deposit: ...   |            |
+ body: (define withdraw ...)   (cond ((eq? m 'withdraw ...    | dispatch: ...  |            |
+       (define deposit ...)      ....  		  	      +----------------|------------+
+       (define dispatch ...)      		  	        ^  ^           |     ^
+       dispatch                   		  	        |  |           V     |
+				  		  	        |  |       .---.---. |
+				  		  	        |  |       | O | O-+-+
+						  	        |  |       `-|-^---'
+                                        	  		|  |         |
+                                        	  		|  |         V
+                                          +---------------------+  |      parameter: amount
+                                          |  	                   |      body: (if (>= blaance amount)
+				 	  |  	                   |            (begin (set! balance (- balance amount))
+				 	  |  	                   |                   balance)
+				 	  |                        |             "Insufficient funds"))
+                                          |                        |
+                                 +--------+-----+           +------+------+
+                           E2 -> | m: 'dispatch |     E3 -> | amount: 40  |
+                                 +--------------+           +-------------+
+                                call to dispatch           call to deposit
+
+
+;; Next, after deposit 60
+((acc 'withdraw) 60)
+
+E2 and E3 have disappeared
+
+           +---------------------------------------------------------------+
+ global -->| make-account:                                                 |
+ env       | acc: ---|------------------+                                  |
+           +---------|------------------|----------------------------------+
+                     V       ^          V        	                 ^
+                 .---.---.   |      .---.---.    	                 |
+      +----------+-O | O-+---+    +-+-O | O-+---+ 	      +----------+------------------+
+      |          `---^---'        | `---^---'   |  	E1 -> | balance: 30                 |
+      V                           V             |             | withdraw: -----+            |
+ parameters: balance           parameters: m	+------------>| deposit: ...   |            |
+ body: (define withdraw ...)   (cond ((eq? m 'withdraw ...    | dispatch: ...  |            |
+       (define deposit ...)      ....  		  	      +----------------|------------+
+       (define dispatch ...)      		  	        ^  ^           |     ^
+       dispatch                   		  	        |  |           V     |
+				  		  	        |  |       .---.---. |
+				  		  	        |  |       | O | O-+-+
+						  	        |  |       `-|-^---'
+                                        	  		|  |         |
+                                        	  		|  |         V
+                                          +---------------------+  |      parameter: amount
+                                          |  	                   |      body: (if (>= blaance amount)
+				 	  |  	                   |            (begin (set! balance (- balance amount))
+				 	  |  	                   |                   balance)
+				 	  |                        |             "Insufficient funds"))
+                                          |                        |
+                                 +--------+-----+           +------+------+
+                           E4 -> | m: 'dispatch |     E5 -> | amount: 60  |
+                                 +--------------+           +-------------+
+                                call to dispatch           call to withdraw
+
+;; Next, defined acc2
+(define acc2 (make-account 100))
+
+           +------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+ global -->| make-account:                                                                                                                                                                |
+ env       | acc: ---|------------------+                                                                                                                                                 |
+           | acc2: --|------------------|--------------------------------------------------------------------------------------------+                                                    |
+           +---------|------------------|--------------------------------------------------------------------------------------------|----------------------------------------------------+
+                     V       ^          V        	                 ^                                                           V                                ^
+                 .---.---.   |      .---.---.    	                 |						         .---.---.                            |
+      +----------+-O | O-+---+    +-+-O | O-+---+ 	      +----------+------------------+                                  +-+-O | O-+---+             +----------+------------------+
+      |          `---^---'        | `---^---'   |  	E1 -> | balance: 30                 |				       | `---^---'   |       E2 -> | balance: 100                |
+      V                           V             |             | withdraw: -----+            |				       V             |             | withdraw: -----+            |
+ parameters: balance           parameters: m	+------------>| deposit: ...   |            |				    parameters: m    +------------>| deposit: ...   |            |
+ body: (define withdraw ...)   (cond ((eq? m 'withdraw ...    | dispatch: ...  |            |				    (cond ((eq? m 'withdraw ...    | dispatch: ...  |            |
+       (define deposit ...)      ....  		  	      +----------------|------------+				      ....                         +----------------|------------+
+       (define dispatch ...)      				               |     ^					                                                    |     ^
+       dispatch                   					       V     |					                                                    V     |
+				  					   .---.---. |					                                                .---.---. |
+				  					   | O | O-+-+					                                                | O | O-+-+
+									   `-|-^---'					                                                `-|-^---'
+                                        				     |						                                                  |
+                                        				     V						                                                  V
+                                        				  parameter: amount				                                               parameter: amount
+                                        				  body: (if (>= blaance amount)			                                               body: (if (>= blaance amount)
+				 					        (begin (set! balance (- balance amount))                                                     (begin (set! balance (- balance amount))
+				 					               balance)				                                                            balance)
+				 					         "Insufficient funds"))			                                                      "Insufficient funds"))
+
+
+;; acc and acc2 share only the global env and the body of MAKE-ACCOUNT.
+
