a167eb72cc14e6a002f20b8ce0be3883cf1761a9
Kazuya Numata <numata@gmail.com>
2013-05-05 18:33:38 +0900

    Implement ch3.3.1's examples

---
+++ ch3/examples/3-3-1.scm
@@ -0,0 +1,21 @@
+(define x '((a b) c d))
+(define y '(e f))
+(set-car! x y)
+x
+
+(define x '((a b) c d))
+(define y '(e f))
+(define z (cons y (cdr x)))
+z
+
+(define x '((a b) c d))
+(define y '(e f))
+
+(set-cdr! x y)
+x
+
+(define (cons2 x y)
+  (let ((new (get-new-pair)))
+    (set-car! new x)
+    (set-cdr! new y)
+    new))
