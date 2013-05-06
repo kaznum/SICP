b99c2355ea6d912063b4f4fd1b88d0dbc8e61f60
Kazuya NUMATA <numata@gmail.com>
2013-05-06 20:16:42 +0900

    Implement ch3.3.1's examples (in progress)

--- ch3/examples/3-3-1.scm
+++ ch3/examples/3-3-1.scm
@@ -22,3 +22,30 @@ x
 
 ;; Sharing and Identity
 
+(define x (list 'a 'b))
+
+(define z1 (cons x x))
+z1
+
+(define z2 (cons (list 'a 'b) (list 'a 'b)))
+z2
+
+(define (set-to-wow! x)
+  (set-car! (car x) 'wow)
+  x)
+
+z1
+;; ((a b) a b)
+
+(set-to-wow! z1)
+;; ((wow b) wow b)
+
+z2
+;; ((a b) a b)
+(set-to-wow! z2)
+;; ((wow b) a b)
+
+;; Mutation is just assignment
+
+;; to be continued
+
