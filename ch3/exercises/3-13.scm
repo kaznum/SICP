296a36ef30dcc7e8e77d582bf6278fb5f7aee7fb
Kazuya NUMATA <numata@gmail.com>
2013-05-06 16:09:17 +0900

    Answer ex3.13

---
+++ ch3/exercises/3-13.scm
@@ -0,0 +1,24 @@
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+(define z (make-cycle (list 'a 'b 'c)))
+
+
+     +---+---+     +---+---+     +---+---+
+z -->| * | *-+---> | * | *-+---> | * | *-+---+
+  +->+-|-+---+     +-|-+---+     +-|-+---+   |
+  |    V             V             V         |
+  |  +---+         +---+         +---+       |
+  |  | a |         | b |         | c |       |
+  |  +---+         +---+         +---+       |
+  |                                          |
+  +------------------------------------------+
+
+(last-pair z)
+;; becomes the infinite loop.
