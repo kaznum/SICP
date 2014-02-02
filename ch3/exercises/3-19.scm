a5f86ca640e17101c64ab253e15c0efabb6710fe
Kazuya Numata <numata@gmail.com>
2014-02-03 01:32:37 +0900

    Fix algorithm in ex3-19

--- ch3/exercises/3-19.scm
+++ ch3/exercises/3-19.scm
@@ -1,52 +1,43 @@
-;; Do not create a pair list
-;; Check whether the current pair occurs in the pairs following it.
+;;; There are two pointers, and the one traverses by double speed of the other
+;;; If there is a loop, sometime, they meet each other.
+;;; But this model ignores when there is a loop in the 'car' element.
 
-(define (occur-again? me exp)
-  (cond ((not (pair? me)) false)
-	((not (pair? exp)) false)
-	((null? me) false)
-	((null? exp) false)
-	((eq? me exp) true)
-	(else
-	 (or
-	  (occur-again? me (car exp))
-	  (occur-again? me (cdr exp))
-	  (loop-exists? (car me))
-	  (loop-exists? (cdr me))))))
-
-(define (loop-exists? exp)
-  (cond ((not (pair? exp)) false)
-	((null? exp) false)
-	(else
-	 (or
-	  (occur-again? exp (car exp))
-	  (occur-again? exp (cdr exp))))))
+(define (cycle? exp)
+  (define (same? singlestep-ptr doublestep-ptr)
+    (cond ((null? singlestep-ptr) false)
+	  ((null? doublestep-ptr) false)
+	  ((null? (cdr doublestep-ptr)) false)
+	  ((eq? singlestep-ptr doublestep-ptr) true)
+	  ((eq? singlestep-ptr (cdr doublestep-ptr)) true)
+	  (else
+	   (same? (cdr singlestep-ptr) (cddr doublestep-ptr)))))
+  (same? exp (cdr exp)))
 
 
-(loop-exists? '(a b c d))
+;;; TEST
+
+(cycle? '(a b c d))
 ;Value: #f
 
 ;;; loop in cdr
 (define p1 '(x y z))
-(loop-exists? (list 'a p1 p1))
+(cycle? (list 'a p1 p1))
 ;Value: #f
 (set-cdr! (cddr p1) p1)
-(loop-exists? p1)
+(cycle? p1)
 ;Value: #t
 
 ;;; loop in car
 (define p2 '(x y z))
-(loop-exists? p2)
+(cycle? p2)
 ;Value: #f
 (set-car! (cdr p2) p2)
+(cycle? p2)
+;Value: #f (Ignore 'car' elements)
 
-(loop-exists? p2)
-;Value: #t
 
-
-;; TODO
 ;; This becomes infinite loop
 ;; When the elements (not the first element) is refered by the end of the list
 (define x (list 'a 'b))
 (set-cdr! (cdr x) (cdr x))
-(loop-exists? x)
+(cycle? x)
