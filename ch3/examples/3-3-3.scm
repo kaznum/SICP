b8a431ead09970835d95d31d3cf7c4edcc895020
Kazuya NUMATA <numata@gmail.com>
2014-02-04 19:15:10 +0900

    Implement ch3.3.3's examples (in progress)

--- ch3/examples/3-3-3.scm
+++ ch3/examples/3-3-3.scm
@@ -1,1 +1,36 @@
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+	(cdr record)
+	false)))
+
+(define (assoc key records)
+  (cond ((null? records) false)
+	((equal? key (caar records)) (car records))
+	(else (assoc key (cdr records)))))
+
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+	(set-cdr! record value)
+	(set-cdr! table
+		  (cons (cons key value) (cdr table)))))
+  'ok)
+
+(define (make-table)
+  (list '*table*))
+
+;; (define onedim (make-table))
+;; (insert! 'a 1 onedim)
+;; (insert! 'b 2 onedim)
+;; (insert! 'c 3 onedim)
+
+;; onedim
+
+;; (lookup 'a onedim)
+;; (lookup 'b onedim)
+;; (lookup 'c onedim)
+
+;;; Two-dimensional tables
+
 ; to be continued
