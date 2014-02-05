0bf77a59e13025adbb7c566878678d796e789120
Kazuya NUMATA <numata@gmail.com>
2014-02-05 12:18:36 +0900

    Make assoc function local in make-table
    This makes calling assoc not to need 'same-key?' as parameter

--- ch3/exercises/3-24.scm
+++ ch3/exercises/3-24.scm
@@ -1,23 +1,23 @@
-(define (assoc key records same-key?)
-  (cond ((null? records) false)
-	((same-key? key (caar records)) (car records))
-	(else (assoc key (cdr records) same-key?))))
-
 (define (make-table same-key?)
+  (define (assoc key records)
+    (cond ((null? records) false)
+	  ((same-key? key (caar records)) (car records))
+	  (else (assoc key (cdr records)))))
+  ;; there is no change from ch3.3.3's examples
   (let ((local-table (list '*table*)))
     (define (lookup key-1 key-2)
-      (let ((subtable (assoc key-1 (cdr local-table) same-key?)))
-    (if subtable
-	(let ((record (assoc key-2 (cdr subtable) same-key?)))
-	  (if record
-	      (cdr record)
-	      false))
-	false)))
+      (let ((subtable (assoc key-1 (cdr local-table))))
+	(if subtable
+	    (let ((record (assoc key-2 (cdr subtable))))
+	      (if record
+		  (cdr record)
+		  false))
+	    false)))
 
     (define (insert! key-1 key-2 value)
-      (let ((subtable (assoc key-1 (cdr local-table) same-key?)))
+      (let ((subtable (assoc key-1 (cdr local-table))))
 	(if subtable
-	    (let ((record (assoc key-2 (cdr subtable) same-key?)))
+	    (let ((record (assoc key-2 (cdr subtable))))
 	      (if record
 		  (set-cdr! record value)
 		  (set-cdr! subtable
@@ -36,15 +36,17 @@
 (define operation-table (make-table (lambda (x y) (equal? x y))))
 (define get (operation-table 'lookup-proc))
 (define put (operation-table 'insert-proc!))
+
+;; TEST
 (put 'sub1 'a 1)
 (put 'sub1 'b 2)
 (put 'sub2 'c 1)
 (put 'sub2 'd 2)
 
 (get 'sub1 'a)
-;; 1
+;;1
 (get 'sub2 'd)
-;; 2
+;;2
 (get 'sub1 'c)
-;; #f
+;;#f
 
