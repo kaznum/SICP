4bbcf0890ae67ed8081a6837167a672bc903f53c
Kazuya NUMATA <numata@gmail.com>
2014-02-06 18:14:18 +0900

    Implement ch3.3.4's examples (in progress)

--- ch3/examples/3-3-4.scm
+++ ch3/examples/3-3-4.scm
@@ -89,6 +89,56 @@
   ((wire 'add-action!) action-procedure))
 
 ;;; The agenda
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time (the-agenda)))
+		  action
+		  the-agenda))
 
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+	(first-item)
+	(remove-first-agenda-item! the-agenda)
+	(propagate))))
+
+;;; A sample simuration
+
+(define (probe name wire)
+  (add-action! wire
+	       (lambda ()
+		 (newline)
+		 (display name)
+		 (display " ")
+		 (display (current-time the-agenda))
+		 (display " New-value= ")
+		 (display (get-signal wire)))))
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+(probe 'sum sum)
+;; executed probe
+(probe carry carry)
+;; executed probe
+
+(half-adder input-1 input-2 sum carry)
+(set-signal! input-1 1)
+
+(propagate)
+;; probe is executed
+
+(set-signal! input-2 1)
+(propagate)
+;; probe is executed
+
+;;; Implementing the agenda
 
 ;; to be continued
