7e8b9bfc43a1028e4ca754bbe037cf54003e5758
Kazuya Numata <numata@gmail.com>
2014-02-09 20:47:52 +0900

    Implement ch3.3.5's examples (in progress)

--- ch3/examples/3-3-5.scm
+++ ch3/examples/3-3-5.scm
@@ -141,5 +141,39 @@
   me)
 
 ;;; Reresenting connectors
+(define (make-connector)
+  (let ((value false) (informant false) (constraints '()))
+    (define (set-my-value newval setter)
+      (cond ((not (has-value? me))
+	     (set! value newval)
+	     (set! informant setter)
+	     (for-each-except setter
+			      inform-about-value
+			      constraints))
+	    ((not (= value newval))
+	     (error "Contradiction" (list value newval)))
+	    (else 'ignored)))
+    (define (forget-my-value retractor)
+      (if (eq? retractor informant)
+	  (begin (set! informant false)
+		 (for-each-except retractor
+				  inform-about-no-value
+				  constraints))
+	  'ignored))
+    (define (connect new-contraint)
+      (if (not (memq new-contraint constraints))
+	  (set! constraints (cons new-contraint constraints)))
+      (if (has-value? me)
+	  (inform-about-value new-constraint))
+      'done)
+    (define (me request)
+      (cond ((eq? request 'has-value?)
+	     (if informant true false))
+	    ((eq? request 'value) value)
+	    ((eq? request 'set-value!) set-my-value)
+	    ((eq? request 'connect) connect)
+	    (else (error "Unknown operation -- CONNECTOR" request))))
+    me))
+
 
 ;; to be continued
