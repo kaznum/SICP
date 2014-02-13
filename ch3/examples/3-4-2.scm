10fb9aead4e9bd24dbb03cacc4f8c770b34dbe06
Kazuya Numata <numata@gmail.com>
2014-02-14 01:13:55 +0900

    Implement c3.4.1's examples (in progress)

--- ch3/examples/3-4-2.scm
+++ ch3/examples/3-4-2.scm
@@ -1,5 +1,16 @@
 ;; Serializing access to shared state
 
 ;; Serializers in Scheme
+;; (define x 10)
+;; (parallel-execute
+;;  (lambda () (set! x (* x x)))
+;;  (lambda () (set! x (+ x 1))))
+
+(define x 10)
+(define s (make-serializer))
+(parallel-execute
+ (s (lambda () (set! x (* x x))))
+ (s (lambda () (set! x (+ x 1))))
+
 
 ;; to be continued
