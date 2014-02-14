5bfb0e55f49624be1c8f84a196b0fbb03bcc1849
Kazuya NUMATA <numata@gmail.com>
2014-02-14 18:49:04 +0900

    Implement ch3.4.2's examples (in progress)

--- ch3/examples/3-4-2.scm
+++ ch3/examples/3-4-2.scm
@@ -31,6 +31,11 @@
     dispatch))
 
 ;; Complexity of using multiple shared resources
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+		       (account2 'balance))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
 
 
 ;; to be continued
