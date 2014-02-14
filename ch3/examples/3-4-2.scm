5ccd50f337859aec29d07e9667c2cbf72905186e
Kazuya NUMATA <numata@gmail.com>
2014-02-14 12:24:21 +0900

    Implement ch3.4.2's examples (in progress)

--- ch3/examples/3-4-2.scm
+++ ch3/examples/3-4-2.scm
@@ -12,5 +12,25 @@
  (s (lambda () (set! x (* x x))))
  (s (lambda () (set! x (+ x 1))))
 
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+	(begin (set! balance (- balance amount))
+	       balance)
+	"Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+	    ((eq? m 'deposit) (protected deposit))
+	    ((eq? m 'balance) balance)
+	    (else (error "Unknown request -- MAKE-ACCOUNT"
+			 m))))
+    dispatch))
+
+;; Complexity of using multiple shared resources
+
 
 ;; to be continued
