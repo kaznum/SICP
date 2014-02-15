79c3d8333d5922702380c40b39ee2f496a85a844
Kazuya Numata <numata@gmail.com>
2014-02-15 12:12:49 +0900

    Implement c3.4.2's examples (in progress)

--- ch3/examples/3-4-2.scm
+++ ch3/examples/3-4-2.scm
@@ -67,5 +67,31 @@
      account1 account2)))
 
 ;; Implementing serializers
+(define (make-serializer)
+  (let ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serialized-p . args)
+	(mutex 'acquire)
+	(let ((val (apply p args)))
+	  (mutex 'release)
+	  val))
+      serialized-p)))
+
+(define (make-mutex)
+  (let ((cell (list false)))
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+	     (if (test-and-set! cell)
+		 (the-mutex 'acquire)))
+	    ((eq? m 'release) (clear! cell))))
+    the-mutex))
+
+(define (clear! cell) (set-car! cell false))
+
+(define (test-and-set! cell)
+  (if (car cell) true (begin (set-car! cell true) false)))
+
+
+;; Deadlock
 
 ;; to be continued
