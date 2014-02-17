548396c29b66428dbd0883f79332f6047be41c5a
Kazuya NUMATA <numata@gmail.com>
2014-02-17 17:06:39 +0900

    Implement ch3.5.1's examples (in progress)

--- ch3/examples/3-5-1.scm
+++ ch3/examples/3-5-1.scm
@@ -28,4 +28,47 @@ stream-for-each
 
 ;;; The stream implementation in action
 
+;;;;;;; definition of prime? from ch1
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+;;;;;;;; end of the definition of prime?
+
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(stream-car
+ (stream-cdr
+  (stream-filter prime?
+		 (stream-enumerate-interval
+		  10000 1000000))))
+
+
+;; (define (stream-filter pred stream)
+;;   (cond ((stream-null? stream) the-empty-stream)
+;; 	((pred (stream-car stream))
+;; 	 (cons-stream (stream-car stream)
+;; 		      (stream-filter
+;; 		       pred
+;; 		       (stream-cdr stream))))
+;; 	(else (stream-filter pred (stream-cdr stream)))))
+
+
+;;; Implementing delay and force
+
 ;; to be continued
