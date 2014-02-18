269a05e3a47a66a6bdb2b327b2d4fd0de0276799
Kazuya NUMATA <numata@gmail.com>
2014-02-18 14:25:44 +0900

    Implement ch3.5.2's examples

--- ch3/examples/3-5-2.scm
+++ ch3/examples/3-5-2.scm
@@ -27,5 +27,48 @@
 (stream-ref primes 50)
 
 ;;; Defining streams implicitly
+(define ones (cons-stream 1 ones))
 
-;; to be continued
+(define (add-streams s1 s2) (stream-map + s1 s2))
+
+(define integers
+  (cons-stream 1 (add-streams ones integers)))
+
+;; (cons 1 (map + (1 1 1...) (cons 1 (map + (1 1 1 ...) (cons 1 (map + (1 1 1)....
+;; (cons 1 (cons 2 (map + (1 1 1...) (map + (1 1 1 ...) (cons 1 (map + (1 1 1 ...
+;; (cons 1 (cons 2 (cons 3 (map ...
+
+(define fibs
+  (cons-stream
+   0
+   (cons-stream 1 (add-streams (stream-cdr fibs) fibs))))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor))
+	      stream))
+
+(define double (cons-stream 1 (scale-stream double 2)))
+
+(stream-car double)
+;; 1
+(stream-car (stream-cdr double))
+;; 2
+(stream-car (stream-cdr (stream-cdr double)))
+;; 4
+(stream-car (stream-cdr (stream-cdr (stream-cdr double))))
+;; 8
+
+(define primes
+  (cons-stream
+   2
+   (stream-filter prime? (integers-starting-from 3))))
+
+(define (prime? n)
+  (define (iter ps)
+    (cond ((> (square (stream-car ps)) n) true)
+	  ((divisible? n (stream-car ps)) false)
+	  (else (iter (stream-cdr ps)))))
+  (iter primes))
+
+(stream-ref primes 5)
+(stream-ref primes 10)
