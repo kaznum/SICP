b301649e6b8dcd8809b8996e8a955511d72ea938
Kazuya NUMATA <numata@gmail.com>
2014-02-18 11:34:57 +0900

    Implement ch3.5.2's examples (in progress)

---
+++ ch3/examples/3-5-2.scm
@@ -0,0 +1,31 @@
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+(define integers (integers-starting-from 1))
+
+(define (divisible? x y) (= (remainder x y) 0))
+(define no-sevens
+  (stream-filter (lambda (x) (not (divisible? x 7)))
+		 integers))
+
+(stream-ref no-sevens 100)
+
+(define (fibgen a b) (cons-stream a (fibgen b (+ a b))))
+(define fib (fibgen 0 1))
+
+(stream-ref fib 100)
+
+(define (sieve stream)
+  (cons-stream
+   (stream-car stream)
+   (sieve (stream-filter
+	   (lambda (x)
+	     (not (divisible? x (stream-car stream))))
+	   (stream-cdr stream)))))
+
+(define primes (sieve (integers-starting-from 2)))
+
+(stream-ref primes 50)
+
+;;; Defining streams implicitly
+
+;; to be continued
