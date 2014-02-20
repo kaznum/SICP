59cee9bd5681aa9783d3875ea85b66ef44e2a65f
Kazuya Numata <numata@gmail.com>
2014-02-20 22:45:47 +0900

    Add another answer to ex3.59 (a)

--- ch3/exercises/3-59.scm
+++ ch3/exercises/3-59.scm
@@ -1,10 +1,28 @@
 ;; a.
-
+;;; Answer 1
 (define (integrate-series c)
   (define (powered-coefficient c n)
     (cons-stream (/ (stream-car c) n)
 		 (powered-coefficient (stream-cdr c) (+ n 1))))
   (powered-coefficient c 1))
+
+;;; Answer 2
+;;; some function has already defined in previous sections and exercises
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+(define integers
+  (integers-starting-from 1))
+(define (mul-streams s1 s2)
+  (if (or (stream-null? s1) (stream-null? s2))
+      the-empty-stream
+      (cons-stream (* (stream-car s1) (stream-car s2))
+		   (mul-streams (stream-cdr s1) (stream-cdr s2)))))
+;;; the Answer is
+(define (invert-stream s)
+  (stream-map (lambda (x) (/ 1 x)) s))
+
+(define (integrate-series c)
+  (mul-streams c (invert-stream integers)))
 
 ;; test
 (define s (cons-stream 10 (cons-stream 11 (cons-stream 12 (cons-stream 13 the-empty-stream)))))
@@ -19,7 +37,6 @@
 ;; 13/4
 
 ;; b.
-
 (define exp-series
   (cons-stream 1 (integrate-series exp-series)))
 
@@ -42,12 +59,3 @@
 (stream-ref sine-series 3)
 (stream-ref sine-series 4)
 (stream-ref sine-series 5)
-
-  
-
-
-
-
-
-
-
