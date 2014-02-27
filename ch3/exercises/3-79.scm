227b92c66c6278d4566605bd0bab2f788d0e7c9a
Kazuya NUMATA <numata@gmail.com>
2014-02-27 12:29:04 +0900

    Answer ex3.79

---
+++ ch3/exercises/3-79.scm
@@ -0,0 +1,33 @@
+;; from text
+(define (stream-map2 f s1 s2)
+  (cons-stream (f (stream-car s1) (stream-car s2))
+	       (stream-map2 f (stream-cdr s1) (stream-cdr s2))))
+
+(define (scale-stream s m)
+  (stream-map (lambda (x) (* m x)) s))
+
+(define (add-streams s1 s2)
+  (stream-map2 (lambda (x y) (+ x y)) s1 s2))
+
+(define (integral delayed-integrand initial-value dt)
+  (define int
+    (cons-stream
+     initial-value
+     (let ((integrand (force delayed-integrand)))
+       (add-streams (scale-stream integrand dt) int))))
+  int)
+
+(define (solve-2nd f dt y0 dy0)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) dy0 dt))
+  (define ddy (stream-map f dy y))
+  y)
+
+
+;; for (f(dy/dt, y) = y)
+;;  this means y = exp^y
+;; y0 = 1
+;; dy0 = 1
+
+(stream-ref (solve-2nd (lambda (dy y) y) 0.0001 1 1) 10000)
+;; 2.7181459268252266
