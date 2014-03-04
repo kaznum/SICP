25851efd51991b3111d5f60dff7faa8db1ecbedd
Kazuya NUMATA <numata@gmail.com>
2014-03-04 14:42:16 +0900

    Implement ch4.1.1's examples (in progress)

--- ch4/examples/4-1-1.scm
+++ ch4/examples/4-1-1.scm
@@ -52,4 +52,11 @@
 
 ;;; Sequences
 
+;; (define (eval-sequence exps env)
+;;   (cond ((last-exp? exps)
+;; 	 (eval (first-exp exps) env))
+;; 	(else
+;; 	 (eval (first-exp exps) env)
+;; 	 (eval-sequence (rest-exps exps) env))))
+
 ;; to be continued
