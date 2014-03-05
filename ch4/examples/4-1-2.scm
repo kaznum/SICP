ab65874ea517d66700e7dd471e0fc927a82db759
Kazuya Numata <numata@gmail.com>
2014-03-05 09:04:53 +0900

    Implement ch4.1.2's examples (in progress)

---
+++ ch4/examples/4-1-2.scm
@@ -0,0 +1,18 @@
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+	((string? exp) true)
+	(else false)))
+
+(define (variable? exp) (symbol? exp))
+
+(define (quoted? exp) (tagged-list? exp 'quote))
+(define (text-of-quotation exp) (caddr exp))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+(define (assignment? exp) (tagged-list? exp 'set!))
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp))
