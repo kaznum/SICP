1c61a3dde2ae6304dd0bf8b5a6718d368a1c65fc
Kazuya Numata <numata@gmail.com>
2014-03-08 22:28:16 +0900

    Answer ex4.2

---
+++ ch4/exercises/4-2.scm
@@ -0,0 +1,28 @@
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+	((variable? exp) (lookup-variable-value exp env))
+	((quoted? exp) (text-of_quotation exp))
+	((application? exp)
+	 (apply (eval (operator exp) env)
+		(list-of-values (operands exp) env)))
+	((assignment? exp) (eval-assignment exp env))
+	((definition? exp) (eval-definition exp env))
+	((if? exp) (eval-if exp env))
+	((lambda? exp) (make-procedure (lambda-parameters exp)
+				       (lambda-body exp)
+				       env))
+	((begin? exp)
+	 (eval-sequence (begin-actions exp) env))
+	((cond? exp) (eval (cond->if exp) env))
+	(else
+	 (error "Unknown expression type: EVAL" exp))))
+
+;;; a.
+;; (define x 3)
+;; 'define' is interpreted as procedure but it isn't so it does
+;; not work well.
+
+;;; b.
+(define (application? exp) (tagged-list? exp 'call))
+(define (operator exp) (cadr exp))
+(define (operands exp) (cddr exp))
