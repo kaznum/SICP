97bc736c34ea549110ed0a5cf811e27e84112895
Kazuya Numata <numata@gmail.com>
2014-03-08 09:26:08 +0900

    Implement ch4.1.2's examples (in progress)

--- ch4/examples/4-1-2.scm
+++ ch4/examples/4-1-2.scm
@@ -35,3 +35,15 @@
 
 (define (make-lambda paramters body)
   (cons' lambda (cons paramters body)))
+
+(define (if? exp) (tagged-list? exp 'if))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
