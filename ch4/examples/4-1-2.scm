af45d1843a42e0ec9f82de77fd078efb46969f5d
Kazuya Numata <numata@gmail.com>
2014-03-08 09:46:00 +0900

    Implement ch4.1.2's examples

--- ch4/examples/4-1-2.scm
+++ ch4/examples/4-1-2.scm
@@ -69,4 +69,24 @@
 
 ;;; Derived expressions
 
-;; to be continued
+(define (cond? exp) (tagged-list? exp 'cond))
+(define (cond-clauses exp) (cdr exp))
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+(define (cond-predicate clause) (car clause))
+(define (cond-actions clause) (cdr clause))
+(define (cond-if exp) (expand-clauses (cond-clause exp)))
+(define (expand-clauses clause)
+  (if (null? clause)
+      'false
+      (let ((first (car clauses))
+	    (rest (cdr clauses)))
+	(if (cond-else-clause? first)
+	    (if (null? rest)
+		(sequence->exp (cond-action first))
+		(error "ELSE clause isn't last: COND->IF"
+		       clauses))
+	    (make-if (cond-predicate first)
+		     (sequence->exp (cond-actions first))
+		     (expand-clauses rest))))))
+
