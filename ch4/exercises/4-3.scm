3fdb957abad9de919b747302effcb4f524f50491
Kazuya NUMATA <numata@gmail.com>
2014-03-10 21:16:07 +0900

    Answer ex4.3

---
+++ ch4/exercises/4-3.scm
@@ -0,0 +1,185 @@
+;; from text
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+	((string? exp) true)
+	(else false)))
+
+(define (variable? exp) (symbol? exp))
+
+;; (define (quoted? exp) (tagged-list? exp 'quote))
+(define (text-of-quotation exp) (caddr exp))
+
+;; (define (tagged-list? exp tag)
+;;   (if (pair? exp)
+;;       (eq? (car exp) tag)
+;;       false))
+
+;; (define (assignment? exp) (tagged-list? exp 'set!))
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp))
+
+;; (define (definition? exp) (tagged-list? exp 'define))
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp)   ;; formal parameters
+		   (cddr exp)))) ;; body
+
+;; (define (lambda? exp) (tagged-list? exp 'lambda))
+(define (lambda-paramters exp) (cadr exp))
+(define (lambda-body exp) (cddr exp))
+
+(define (make-lambda paramters body)
+  (cons' lambda (cons paramters body)))
+
+;; (define (if? exp) (tagged-list? exp 'if))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+;; (define (begin? exp) (tagged-list? exp 'begin))
+(define (begin-actions exp) (cdr exp))
+(define (last-exp? seq) (null? (cdr seq)))
+(define (first-exp seq) (car seq))
+(define (rest-exps seq) (cdr seq))
+
+(define (sequence-exp seq)
+  (cond ((null? seq) seq)
+	((last-exp? seq) (first-exp seq))
+	(else (make-begin seq))))
+
+(define (make-begin seq) (cons 'begin seq))
+
+(define (application? exp) (pair? exp))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+(define (no-operands? ops) (null? ops))
+(define (first-operand ops) (car ops))
+(define (rest-operands ops) (cdr ops))
+
+;;; Derived expressions
+
+;; (define (cond? exp) (tagged-list? exp 'cond))
+(define (cond-clauses exp) (cdr exp))
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+(define (cond-predicate clause) (car clause))
+(define (cond-actions clause) (cdr clause))
+(define (cond-if exp) (expand-clauses (cond-clause exp)))
+(define (expand-clauses clause)
+  (if (null? clause)
+      'false
+      (let ((first (car clauses))
+	    (rest (cdr clauses)))
+	(if (cond-else-clause? first)
+	    (if (null? rest)
+		(sequence->exp (cond-action first))
+		(error "ELSE clause isn't last: COND->IF"
+		       clauses))
+	    (make-if (cond-predicate first)
+		     (sequence->exp (cond-actions first))
+		     (expand-clauses rest))))))
+
+
+;; Apply
+
+(define (apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+	 (apply-primitive-procedure procedure arguments))
+	((compound-procedure? procedure)
+	 (eval-sequence
+	  (procedure-body procedure)
+	  (extend-environment
+	   (procedure-parameters procedure)
+	   arguments
+	   (procedure-environment procedure))))
+	(else
+	 (error
+	  "Unknown procedure type: APPLY" procedure))))
+
+;; Procedure arguments
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+	    (list-of-values (rest-operands exps) env))))
+
+;; Conditionals
+
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+;; Sequences
+
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps)
+	 (eval (first-exp exps) env))
+	(else
+	 (eval (first-exp exps) env)
+	 (eval-sequence (rest-exps exps) env))))
+
+;; Assignments and definitions
+
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+		       (eval (assignment-value exp) env)
+		       env)
+  'ok)
+
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+    (eval (definition-value exp) env)
+    env)
+  'ok)
+
+
+;; Answer
+;; Do not use 'tagged-list?'
+(define ops (make-hash-table))
+(define (put! key op) (hash-table-set! ops key op))
+(define (get key) (hash-table-ref ops key))
+
+(define (install-operations)
+  (put! 'quote
+	(lambda (exp env)
+	  (text-of-quotation exp)))
+  (put! 'set! eval-assignment exp)
+  (put! 'lambda
+	(lambda (exp env)
+	  (make-procedure (lambda-parameters exp) (lambda-body exp) env)))
+  (put! 'define eval-definition)
+  (put! 'begin
+	(lambda (exp env)
+	  (eval-sequence (begin-actions exp) env)))
+  (put! 'if eval-if)
+  (put! 'cond
+	(lambda (exp env)
+	  (eval (cond->if exp) env)))
+  'done)
+(install-operations)
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+	((variable? exp) (lookup-variable-value exp env))
+	((get (car exp))
+	 ((get (car exp)) exp env))
+	((application? exp)
+	 (apply (eval (operator exp) env)
+		(list-of-values (operands exp) env)))
+	(else
+	 (error "Unknown expression type: EVAL" exp))))
+
