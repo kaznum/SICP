8af8041b02ec7d7e90338a08f361831add486a50
Kazuya NUMATA <numata@gmail.com>
2014-03-22 14:18:36 +0900

    Fix spelling

--- ch4/examples/4-1-3.scm
+++ ch4/examples/4-1-3.scm
@@ -8,7 +8,7 @@
   (list 'procedure parameters body env))
 (define (compound-procedure? p)
   (tagged-list? p 'procedure))
-(define (procedure-paramters p) (cadr p))
+(define (procedure-parameters p) (cadr p))
 (define (procedure-body p) (caddr p))
 (define (procedure-environment p) (cadddr p))
 
