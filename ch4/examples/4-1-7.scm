705a9be303b90b4f3fed7f18b8a87fd930262a5f
Kazuya NUMATA <numata@gmail.com>
2014-04-04 18:07:40 +0900

    Implement 4.1.7's examples (in progress)

--- ch4/examples/4-1-7.scm
+++ ch4/examples/4-1-7.scm
@@ -2,3 +2,29 @@
 ;;   (if (= n 1) 1 (* (factorial (- n 1)) n)))
 ;; (factorial 4)
 
+;; (define (eval exp env) ((analyze exp) env))
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp) (analyze-self-evaluating exp))
+	((quoted? exp) (analyze-quoted exp))
+	((variable? exp) (analyze-variable exp))
+	((assignment? exp) (analyze-assignment exp))
+	((definition? exp) (analyze-definition exp))
+	((if? exp) (analyze-if exp))
+	((lambda? exp) (analyze-lambda exp))
+	((begin? exp) (analyze-sequence (begin-actions exp)))
+	((cond? exp) (analyze (cond->if exp)))
+	((application? exp) (analyze-application exp))
+	(else (error "Unknown expression type: ANALYZE" exp))))
+
+(define (analyze-self-evaluating exp)
+  (lambda (env) exp))
+
+(define (analyze-quoted exp)
+  (let ((qval (text-of-quotation exp)))
+    (lambda (env) qval)))
+
+(define (analyze-variable exp)
+  (lambda (env) (lookup-variable-value exp env)))
+
+;; to be continued
