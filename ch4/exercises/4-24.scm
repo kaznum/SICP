bb89900937569ee983471fd7c9ff029da9a997bd
Kazuya Numata <numata@gmail.com>
2014-04-22 00:24:09 +0900

    Answer ex4.24
    Close #4

---
+++ ch4/exercises/4-24.scm
@@ -0,0 +1,6 @@
+;; Calcurate fib 3000 * 100 times)
+;; See the result of 4-24-analyze.scm and 4-24-eval.scm
+;;
+;; The ratio is 50sec : 86sec
+;; The fraction is 86/50 = 1.72 times
+;; This difference is for redundant syntactic analysis.
