236ec53eec4376026728dac5e3f8f1023daf8e4c
Kazuya NUMATA <numata@gmail.com>
2014-05-03 23:59:18 +0900

    Implement ch4.2.2's examples

--- ch4/examples/4-2-2.scm
+++ ch4/examples/4-2-2.scm
@@ -335,10 +335,11 @@
 
 
 ;;; Representing thunks
-(define (force-it obj)
-  (if (thunk? obj)
-      (actual-value (thunk-exp obj) (thunk-env obj))
-      obj))
+;;;; these code is replaced imediately
+;; (define (force-it obj)
+;;   (if (thunk? obj)
+;;       (actual-value (thunk-exp obj) (thunk-env obj))
+;;       obj))
 
 (define (delay-it exp env)
   (list 'thunk exp env))
@@ -348,4 +349,21 @@
 (define (thunk-env thunk) (caddr thunk))
 
 
-;; to be continued
+(define (evaluated-thunk? obj)
+  (tagged-list? obj 'evaluated-thunk))
+(define (thunk-value evaluated-thunk)
+  (cadr evaluated-thunk))
+
+(define (force-it obj)
+  (cond ((thunk? obj)
+	 (let ((result (actual-value (thunk-exp obj)
+				     (thunk-env obj))))
+	   (set-car! obj 'evaluated-thunk)
+	   (set-car! (cdr obj)
+		     result)
+	   (set-cdr! (cdr obj)
+		     '())
+	   result))
+	((evaluated-thunk? obj) (thunk-value obj))
+	(else obj)))
+
