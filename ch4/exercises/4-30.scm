f29cd4a148aa73a3e2c024651194921b62fa3a54
Kazuya NUMATA <numata@gmail.com>
2014-05-15 16:28:47 +0900

    Answer ex4.30

--- ch4/exercises/4-30.scm
+++ ch4/exercises/4-30.scm
@@ -20,7 +20,46 @@
 ;; 6. 'newline' and 'display' are  primitive functions then they are forced and display a blank line and '57'
 ;; 7. (for-each proc (cdr items)) in the next sequence become thunk at first but 'eval' force it soon and loop from 1.
 
-;; ex-30 b.
+;; ex4-30 b.
+
+(define (p1 x)
+  (set! x (cons x '(2)))
+  x)
+(define (p2 x)
+  (define (p e)
+    e
+    x)
+  (p (set! x (cons x '(2)))))
+
+;;;;; with Text's eval-sequence
+(p1 1)
+;; set! and cons are primitive, then
+;; -> (set! x '(1 2))
+;; -> x ;; '(1 2)
+
+(p2 1)
+;; p is evaluated and
+;; (set! x (cons x '(2))) becomes thunk because p is combined procedure.
+;; (eval e) is called during the process of (p e) and lookup-variable-value returns '(thunk (set! x (cons x '(2))) env) which is not forced.
+;; finary return x as 1 which is the initial argument of (p2 1)
 
 
-;; to be continued
+;;;;; with Cy's eval-sequence
+(p1 1)
+;; The first exp (set! x (cons x '(2))) is forced, then x become '(1 2) .
+;; next exp 'x' is evaluated as '(1 2) and return '(1 2)
+
+(p2 1)
+;; At the exp (p (set! x (cons x '(2))))), p is forced, and  (set! x (cons x '(2)))) becomes thunk.
+;; In the p's process, the first e which is (set! x (cons x '(2))) is forced because Cy's version calls 'actual-value' and x becomes '(1 2)
+;; and last x returns '(1 2)
+
+
+;; ex4-30 c.
+;; (lambda (x) (newline) (display x)) is composed with only primitive functions,
+;; then they are not delayed even with the text version.
+
+
+;; ex4-30 d.
+;; I think Cy's is better, because it is unpredictable whether expressions are forced or delayed only when reading the partial codes, and it is not desirable to have to think whether the focusing function is primitive or not.
+
