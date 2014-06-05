17273d5756f64441480ed4187f1c9d5040f44e08
Kazuya NUMATA <numata@gmail.com>
2014-06-05 16:45:06 +0900

    Implement ch4.3.1's examples (wip)

---
+++ ch4/examples/4-3-1.scm
@@ -0,0 +1,12 @@
+(define (require p) (if (not p) (amb)))
+
+(define (an-element-of items)
+  (require (not (null? items)))
+  (amb (car items) (an-element-of (cdr items))))
+
+(define (an-integer-starting-from n)
+  (amb n (an-integer-starting-from (+ n 1))))
+
+;;; Driver loop
+
+;; to be continued
