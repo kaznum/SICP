00fb5d16591f7a69a015b599b06277c6847989a8
Kazuya NUMATA <numata@gmail.com>
2015-10-16 09:36:12 +0900

    Add some comments to ex4-24

--- ch4/exercises/4-24.scm
+++ ch4/exercises/4-24.scm
@@ -4,3 +4,5 @@
 ;; The ratio is 50sec : 86sec
 ;; The fraction is 86/50 = 1.72 times
 ;; This difference is for redundant syntactic analysis.
+;; The evaluations of (if ..), (= n 1), (factorial ..) and (- n 1)
+;; are called the same times as the loop count on metacircular version.
