0192e5ccec776c1ac897a00699a4c2e248fe47c5
Kazuya NUMATA <numata@gmail.com>
2015-10-18 16:56:04 +0900

    Refine some comments of the solution of ex4.25

--- ch4/exercises/4-25.scm
+++ ch4/exercises/4-25.scm
@@ -3,42 +3,43 @@
 
 (define (factorial n)
   (unless (= n 1)
-	  (* n (factorial (- n 1)))
-	  1))
+          (* n (factorial (- n 1)))
+          1))
+
+;;
+;; applicative-order
+;;
 
 (factorial 5)
 ;;=>
 (unless (= 5 1)
-	(* 5 (factorial (- 5 1))) 1)
+        (* 5 (factorial (- 5 1))) 1)
 ;;=>
 (unless false
-	(* 5 (factorial 4)) 1)
+        (* 5 (factorial 4)) 1)
 
 ;;=>
 (unless false
-	(* 5 (unless false
-		     (* 4 (factorial 3)) 1)) 1)
+        (* 5 (unless false
+                     (* 4 (factorial 3)) 1)) 1)
 ;;=>
 (unless false
-	(* 5 (unless false
-		     (* 4 (unless false
-				  (* 3 (factorial 2))
-				  1)) 1)) 1)
+        (* 5 (unless false
+                     (* 4 (unless false
+                                  (* 3 (factorial 2)) 1)) 1)) 1)
 ;;=>
 (unless false
-	(* 5 (unless false
-		     (* 4 (unless false
-				  (* 3 (unless false
-					       (* 2 (factorial 1))
-					       1)) 1)) 1)) 1)
+        (* 5 (unless false
+                     (* 4 (unless false
+                                  (* 3 (unless false
+                                               (* 2 (factorial 1)) 1)) 1)) 1)) 1)
 ;;=>
 (unless false
-	(* 5 (unless false
-		     (* 4 (unless false
-				  (* 3 (unless false
-					       (* 2 (unless true
-							    (* 1 (factorial 0))
-							    1)) 1)) 1)) 1)) 1)
+        (* 5 (unless false
+                     (* 4 (unless false
+                                  (* 3 (unless false
+                                               (* 2 (unless true
+                                                            (* 1 (factorial 0)) 1)) 1)) 1)) 1)) 1)
 ;;=>
 (unless false
 	(* 5 (unless false
@@ -50,30 +51,26 @@
 									 1)) 1)) 1)) 1)) 1)) 1)
 ;;=> it will not end forever
 
-;; when it is in normal order language,
+;;
+;; normal-order
+;;
+(factorial 5)
+;;=>
 (unless (= n 1)
-	(* n (factorial (- n 1))) 1)
-=>
+        (* n (factorial (- n 1))) 1)
+;; =>
 (if (= n 1)
     1
     (* n (factorial (- n 1))))
 ;; where n = 5
-=>
+;; =>
 ;; (= n 1) is evaluated first where n = 5, then
-(* n (factorial (- n 1)))
-;; where n = 5
-=>
-(* n (if (= (- n 1) 1)
+(* 5 (factorial (- 5 1)))
+;; =>
+(* 5 (if (= (- 5 1) 1)
 	 1
-	 (* (- n 1) (factorial (- (- n 1) 1)))))
-;; where n = 5.
-;; first, second n  and (= (- n 1) 1) is evaluated first, then
-(* 5 (* 4 (factorial (- (- (- n 1) 1) 1))))
+	 (* (- 5 1) (factorial (- (- 5 1) 1)))))
+(* 5 (* 4 (factorial (- (- (- 4 1) 1) 1))))
 (* 5 (* 4 (* 3 (* 2 (* 1 1)))))
 ;; => 120
 
-
-
-
-
-	   
