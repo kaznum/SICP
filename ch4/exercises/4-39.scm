db38add4c0fdf3ed57dd7dfd77c5d16d5b581a2b
Kazuya NUMATA <numata@gmail.com>
2015-11-16 09:12:30 +0900

    Answer ex4.39

---
+++ ch4/exercises/4-39.scm
@@ -0,0 +1,4 @@
+;; It should be effective if the possible conditions are restricted to eliminate the many unnecessory
+;; alternatives as soon as possible because those which follows the previous ones do not need check
+;; these unnecessory alternatives.
+
