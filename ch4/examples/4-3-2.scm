4e94a1928ef139a56d84820d4952349eee7b9652
Kazuya NUMATA <numata@gmail.com>
2015-12-13 15:05:10 +0900

    Implement 4.3.2's examples

--- ch4/examples/4-3-2.scm
+++ ch4/examples/4-3-2.scm
@@ -133,6 +133,23 @@
 ;;             (simple-noun-phrase (article the) (noun class)))))
 
 ;; (parse '(the professor lectures to the student with the cat))
+;; (sentence (simple-noun-phrase (article the) (noun professor))
+;;           (verb-phrase
+;;            (verb-phrase
+;;             (verb lectures)
+;;             (prep-phrase
+;;              (prep to)
+;;              (simple-noun-phrase (article the) (noun student))))
+;;            (prep-phrase
+;;             (prep with)
+;;             (simple-noun-phrase (article the) (noun cat)))))
 
-
-;; to be continued
+;; (sentence (simple-noun-phrase (article the) (noun professor))
+;;           (verb-phrase
+;;            (verb lectures)
+;;            (prep-phrase
+;;             (prep to)
+;;             (noun-phrase (simple-noun-phrase (article the) (noun student))
+;;                          (prep-phrase
+;;                           (prep with)
+;;                           (simple-noun-phrase (article the) (noun cat)))))))
