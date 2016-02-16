c4f823203c38783d0a87881bac55ce3815328455
Kazuya NUMATA <numata@gmail.com>
2016-02-16 09:28:14 +0900

    Add missing definitions to ch4.3.3 example

--- ch4/examples/4-3-3.scm
+++ ch4/examples/4-3-3.scm
@@ -21,8 +21,22 @@
 (define (enclosing-environment env) (cdr env))
 (define (first-frame env) (car env))
 (define the-empty-environment '())
+
+(define (make-frame variables values)
+  (cons variables values))
+
 (define (frame-variables frame) (car frame))
 (define (frame-values frame) (cdr frame))
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons var (car frame)))
+  (set-cdr! frame (cons val (cdr frame))))
+
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments applied" vars vals)
+          (error "Too few arguments applied" vars vals))))
 
 (define (lookup-variable-value var env)
   (define (env-loop env)
@@ -61,7 +75,7 @@
   (let ((frame (first-frame env)))
     (define (scan vars vals)
       (cond ((null? vars)
-             (add-bind-to-frame! var val frame))
+             (add-binding-to-frame! var val frame))
             ((eq? var (car vars)) (set-car! vals val))
             (else (scan (cdr vars) (cdr vals)))))
     (scan (frame-variables frame) (frame-values frame))))
@@ -143,6 +157,50 @@
 (define (true? x) (not (eq? x false)))
 (define (false? x) (eq? x false))
 
+(define (prompt-for-input string)
+  (newline) (newline) (display string) (newline))
+(define (announce-output string)
+  (newline) (display string) (newline))
+
+(define (user-print object)
+  (if (compound-procedure? object)
+      (display (list 'compound-procedure
+                     (procedure-parameters object)
+                     (procedure-body object)
+                     '<procedure-env>))
+      (display object)))
+
+(define (setup-environment)
+  (let ((initial-env
+         (extend-environment (primitive-procedure-names)
+                             (primitive-procedure-objects)
+                             the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+
+(define (primitive-procedure-objects)
+  (map (lambda (proc) (list 'primitive (cadr proc)))
+       primitive-procedures))
+
+(define primitive-procedures
+  (list (list 'car car)
+        (list 'cdr cdr)
+        (list 'cons cons)
+        (list 'null? null?)
+        (list '+ +)
+        (list '* *)
+        (list '= =)
+        (list '- -)
+        (list 'get-universal-time get-universal-time)
+        (list 'display display)
+        (list 'newline newline)
+        ;; more
+        ))
+(define the-global-environment (setup-environment))
 
 ;;; The followings will be redefined in this chapter.
 
