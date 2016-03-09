43564372e6695d2c7a544c84a18f734e139fd5a9
Kazuya NUMATA <numata@gmail.com>
2016-03-09 09:39:00 +0900

    (WIP) Implement ch4.4.1's examples

--- ch4/examples/4-4-1.scm
+++ ch4/examples/4-4-1.scm
@@ -72,4 +72,28 @@
 
 ;;;; Rules
 
+(rule (live-near ?person-1 ?person-2)
+      (address ?person-1 (?town . ?rest-1))
+      (address ?person-2 (?town . ?rest-2))
+      (not (same ?person-1 ?person-2)))
+
+(rule (same ?x ?x))
+
+(rule (wheel ?person)
+      (and (supervisor ?middle-manager ?person)
+           (supervisor ?x ?middle-manager)))
+
+(live-near ?x (Bitdiddle Ben))
+
+(and (job ?x (computer programmer))
+     (live-near ?x (Bitdiddle Ben)))
+
+(rule (outranked-by ?staff-person ?boss)
+      (or (supervisor ?staff-person ?boss)
+          (and (supervisor ?staff-person ?middle-manager)
+               (outranked-by ?middle-manager ?boss))))
+
+
+;;;; Logic as programs
+
 ;; to be continued
