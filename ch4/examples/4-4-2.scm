e3673ac27886d2770be5cdc2624a32c25a96d2ff
Kazuya NUMATA <numata@gmail.com>
2016-03-24 09:23:47 +0900

    (WIP) Implement ch4.4.2's examples

---
+++ ch4/examples/4-4-2.scm
@@ -0,0 +1,8 @@
+;;;; Pattern matching
+
+;; Pattern matcher: Takes as inputs a 'pattern', 'datum' and 'frame'
+;; 'frame': Specifies bindings for various pattern variables
+
+;;;; Streams of frames
+
+;; to be continued
