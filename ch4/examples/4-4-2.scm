56672c31bc9e8f9ed32a7977e92c0834cf8bff99
Kazuya NUMATA <numata@gmail.com>
2016-03-28 09:08:42 +0900

    (WIP) Implement ch4.4.2's examples

--- ch4/examples/4-4-2.scm
+++ ch4/examples/4-4-2.scm
@@ -11,4 +11,27 @@
 
 ;;;; Compound queries
 
+(and (can-do-job ?x (computer programmer trainee))
+     (job ?person ?x))
+
+;; The first predicate of 'and' generates the frames' stream which matchs all ?x candidates.
+;; Then for each frame of the stream, generate and filter the frames where ?x and ?person are binded.
+
+(or A B)
+;; The input frame stream is filtered and extended by each of A and B, then the generated frames by each are merged
+;; to generate the final output stream.
+;; The matching pattern could increase exponentially, which leads to heavy processing.
+
+(and (supervisor ?x ?y)
+     (not (job? ?x (computer programmer))))
+
+;; This generates the extension frames which have ?x ?y bindings.
+;; Then filter by '(job? ?x (computer programmer))' and filter the exntension frames by the result of it.
+
+;; For lisp-value, at first generating the frames which all variables are instantiated then filter
+;; and apply them lisp-value predicate. All input frames are filtered out if the predicate fails.
+
+
+;;;; Unification
+
 ;; to be continued
