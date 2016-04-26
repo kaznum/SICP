9ca123ec67cad059997c0eb1097e04bff328ff53
Kazuya NUMATA <numata@gmail.com>
2016-04-26 09:26:05 +0900

    Implement ch4.4.4.4's examples

--- ch4/examples/4-4-4-4.scm
+++ ch4/examples/4-4-4-4.scm
@@ -26,4 +26,39 @@
             (else exp)))
     (tree-walk rule)))
 
-;; to be continued
+(define (unify-match p1 p2 frame)
+  (cond ((eq? frame 'failed) 'failed)
+        ((equal? p1 p2) frame)
+        ((var? p1) (extend-if-possible p1 p2 frame))
+        ((var? p2) (extend-if-possible p2 p1 frame))
+        ((and (pair? p1) (pair? p2))
+         (unify-match (cdr p1) (cdr p2)
+                      (unify-match (car p1) (car p2) frame)))
+        (else 'failed)))
+
+(define (extend-if-possible var val frame)
+  (let ((binding (binding-in-frame var frame)))
+    (cond (binding (unify-match (binding-value binding) val frame))
+          ((var? val)
+           (let ((binding (binding-in-frame val frame)))
+             (if binding
+                 (unify-match var (binding-value binding) frame)
+                 (extend var val frame))))
+          ((depends-on? val var frame) 'failed)
+          (else (extend var val frame)))))
+
+;; depends-on? test the variable has the value but the value contains the variable itself.
+
+(define (depends-on? exp var frame)
+  (define (tree-walk e)
+    (cond ((var? e)
+           (if (equal? var e)
+               true
+               (let ((b (binding-in-frame e frame)))
+                 (if b
+                     (tree-walk (binding-value b))
+                     false))))
+          ((pair? e)
+           (or (tree-walk (car e)) (tree-walk (cdr e))))
+          (else false)))
+  (tree-walk exp))
