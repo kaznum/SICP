898522f6f32b18f37ffc62c8288008a0d10757c7
Kazuya NUMATA <numata@gmail.com>
2016-05-09 09:06:49 +0900

    (WIP) Implement ch4.4.4.7's examples

---
+++ ch4/examples/4-4-4-7.scm
@@ -0,0 +1,48 @@
+(define (type exp)
+  (if (pair? exp)
+      (car exp)
+      (error "Unknown expression TYPE" exp)))
+
+(define (contents exp)
+  (if (pair? exp)
+      (cdr exp)
+      (error "Unknown expression CONTENTS" exp)))
+
+;; (assert! <rule-or-assertion>)
+
+(define (assertion-to-be-added? exp)
+  (eq? (type exp) 'assert!))
+
+(define (add-assertion-body exp)
+  (car (contents exp)))
+
+;; for and, or, not and lisp-value special forms
+(define (empty-conjunction? exps) (null? exps))
+(define (first-conjunct exps) (car exps))
+(define (rest-conjuncts exps) (cdr exps))
+
+(define (empty-disjunction? exps) (null? exps))
+(define (first-disjunct exps) (car exps))
+(define (rest-disjuncts exps) (cdr exps))
+
+(define (negated-query exps) (car exps))
+
+(define (predicate exps) (car exps))
+(define (args exps) (cdr exps))
+
+;; the syntax of rules
+
+(define (rule? statement)
+  (tagged-list? statement 'rule))
+
+(define (conclusion rule)
+  (cadr rule))
+
+(define (rule-body rule)
+  (if (null? (cddr rule))
+      '(always-true)
+      (caddr rule)))
+
+;; to be continued
+
+
