ec839fdbc7339fb815a4d2dfc5686962f1590932
Kazuya NUMATA <numata@gmail.com>
2016-05-10 09:07:51 +0900

    Implement ch4.4.4.7's examples

--- ch4/examples/4-4-4-7.scm
+++ ch4/examples/4-4-4-7.scm
@@ -43,6 +43,44 @@
       '(always-true)
       (caddr rule)))
 
-;; to be continued
+(define (query-syntax-process exp)
+  (map-over-symbol expand-question-mark exp))
+
+(define (map-over-symbol proc exp)
+  (cons ((pair? exp)
+         (cons (map-over-symbol proc (car exp))
+               (map-over-symbol proc (cdr exp))))
+        ((symbol? exp)
+         (proc exp))
+        (else exp)))
+
+(define (expand-question-mark symbol)
+  (let ((chars (symbol->string symbol)))
+    (if (string=? (substring chars 0 1) "?")
+        (list '? (string->symbol
+                  (substring chars 1 (string-length chars))))
+        symbol)))
+
+(define (var? exp) (tagged-list? exp '?))
+(define (constant-symbol? exp) (symbol? exp))
 
 
+;; Unique variables during rule application
+(define rule-counter 0)
+
+(define (new-rule-application-id)
+  (set! rule-counter (+ 1 rule-counter))
+  rule-counter)
+
+(define (make-new-variable var rule-application-id)
+  (cons '? (cons rule-application-id (cdr var))))
+
+;; print unbound variable
+(define (contract-question-mark variable)
+  (string->symbol
+   (string-append "?"
+                  (if (number? (cadr variable))  ;; for during rule
+                      (string-append (symbol->string (caddr variable))
+                                     "-"
+                                     (number->string (cadr variable)))
+                      (symbol->string (cadr variable))))))
