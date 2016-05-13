113fd7e478bf0064f0f1d55f77d623850c4f9923
Kazuya NUMATA <numata@gmail.com>
2016-05-13 09:38:34 +0900

    Add test sample to 4.4.4

--- ch4/examples/4-4-4-combined.scm
+++ ch4/examples/4-4-4-combined.scm
@@ -7,4 +7,23 @@
 (load "4-4-4-7")
 (load "4-4-4-8")
 
+
+(define sample "
+;; the following is not in infinite loop
+
+(assert! (say bye 1))
+(assert! (say bye 2))
+(assert! (say hello 1))
+(assert! (say hello 2))
+(assert! (rule (disp ?x ?y) (say ?y ?x)))
+(assert! (rule (same ?x ?x)))
+(and (say bye ?x)
+     (not (same ?x 2)))
+(and (say bye ?x) (say hello ?y) (same ?x ?y) (lisp-value > ?x 1))
+(and (say bye ?x) (say hello ?y) (same ?x ?y) (not (lisp-value > ?x 1)))
+(and (say bye ?x) (say hello ?y) (or (lisp-value > ?x 1) (same ?x ?y)))
+
+")
+
+
 (query-driver-loop)
