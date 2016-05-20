b9c1eb3b6bdceda33ac951221c7e2b97ea9d1d16
Kazuya NUMATA <numata@gmail.com>
2016-05-20 10:07:44 +0900

    Add some comments to ch4.4.4.6's examples

--- ch4/examples/4-4-4-6.scm
+++ ch4/examples/4-4-4-6.scm
@@ -20,6 +20,41 @@
         (delay (stream-cdr s1))))))
 
 ;; proc <a frame> generates the multiple streams of frames
+;; The input frame stream (s) is like
+;;   '(( (?x 1) (?y 2) )
+;;     ( (?x 3) (?y 4) ))
+;;     ()
+;;     ( (?x 5) (?y 6) ))
+;;     ( (?x 7) (?y 8) ))
+;;
+;; Then, (stream-map proc s) generates
+;;   '((
+;;      ( (?x 1) (?y 2) (?z 3) )
+;;      ( (?x 1) (?y 2) (?z 4) )
+;;     )
+;;     (
+;;      ( (?x 3) (?y 4) (?z 5) )
+;;      ( (?x 3) (?y 4) (?z 6) )
+;;     )
+;;     ()
+;;     (
+;;      ( (?x 5) (?y 6) (?z 7) )
+;;      ( (?x 5) (?y 6) (?z 8) )
+;;     )
+;;     ()
+;;    )
+;;
+;; Then (flatten-stream <the results above>) generates
+;;
+;;   '(
+;;      ( (?x 1) (?y 2) (?z 3) )
+;;      ( (?x 3) (?y 4) (?z 5) )
+;;      ( (?x 5) (?y 6) (?z 7) )
+;;      ( (?x 1) (?y 2) (?z 4) )
+;;      ( (?x 3) (?y 4) (?z 6) )
+;;      ( (?x 5) (?y 6) (?z 8) )
+;;    )
+
 (define (stream-flatmap proc s)
   (flatten-stream (stream-map proc s)))
 
