54841ada9938498064d230d0716b0372f328c70b
Kazuya NUMATA <numata@gmail.com>
2016-05-23 09:26:29 +0900

    Answer ex4.74

--- ch4/exercises/4-74.scm
+++ ch4/exercises/4-74.scm
@@ -54,11 +54,58 @@
 ;;        (stream-car s)
 ;;        (delay (flatten-stream (stream-cdr s))))))
 
+;;;; a.
 
-(define (simple-stream-flatmap proc s)
-  (simple-flatten (stream-map proc s)))
-(define (simple-flatten stream)
-  (stream-map stream-car
-              (stream-filter (lambda (x) (not (stream-null? x)))
-                             stream)))
-;; to be continued
+;; (define (simple-stream-flatmap proc s)
+;;   (simple-flatten (stream-map proc s)))
+;; (define (simple-flatten stream)
+;;   (stream-map stream-car
+;;               (stream-filter (lambda (x) (not (stream-null? x)))
+;;                              stream)))
+
+
+;; (define (negate operands frame-stream)
+;;   (simple-stream-flatmap
+;;    (lambda (frame)
+;;      (if (stream-null? (qeval (negated-query operands) (singleton-stream frame)))
+;;          (singleton-stream frame)
+;;          the-empty-stream))
+;;    frame-stream))
+;; (put 'not 'qeval negate)
+
+;; (define (lisp-value call frame-stream)
+;;   (simple-stream-flatmap
+;;    (lambda (frame)
+;;      (if (execute
+;;           (instantiate
+;;            call
+;;            frame
+;;            (lambda (v f)
+;;              (error "Unknown pat var: LISP-VALUE" v))))
+;;          (singleton-stream frame)
+;;          the-empty-stream))
+;;    frame-stream))
+;; (put 'lisp-value 'qeval lisp-value)
+
+;; (define (find-assertions pattern frame)
+;;   (simple-stream-flatmap
+;;    (lambda (datum)
+;;      (check-an-assertion datum pattern frame))
+;;    (fetch-assertions pattern frame)))
+
+(define samples "
+
+(assert! (a 1))
+(assert! (a 2))
+(assert! (a 3))
+(assert! (b 1))
+(assert! (b 2))
+(assert! (b 3))
+
+(and (a ?m) (b ?m) (or (lisp-value > ?m 1) (lisp-value < ?m 3)))
+
+")
+
+;;;; b.
+;; I don't think there is any change....
+
