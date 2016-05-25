a10b8fc68e840f15d7e0a867950784816318ee89
Kazuya NUMATA <numata@gmail.com>
2016-05-25 11:06:27 +0900

    (WIP) Answer ex4.75

--- ch4/exercises/4-75.scm
+++ ch4/exercises/4-75.scm
@@ -10,38 +10,96 @@
 
 ;;;; Answer
 
-(define (unique-asserted pattern frame-stream)
+(define (unique-asserted operands frame-stream)
   (define unique-frames the-empty-stream)
+  (define (delete-binding variable frame)
+    (cond ((null? frame)
+           '())
+          ((equal? variable (binding-variable (car frame)))
+           (cdr frame))
+          (else
+           (cond (car frame) (delete-binding variable (cdr frame))))))
+
   (define (equal-frame? f1 f2)
-    (if (and (null? f1) (null? f2))
-        true
-        (let ((binding (car f1)))
-          (let ((var (binding-variable binding))
-                (val (binding-value binding))
-                (frame-in-f2 (binding-in-frame var f2)))
-            (cond ((not frame-in-f2) false)
-                  ((equal? val (binding-value frame-in-f2))
+    (cond ((and (null? f1) (null? f2)) true)
+          ((not (= (length f1) (length f2))) false)
+          (else
+           (let ((binding (car f1)))
+             (let ((var (binding-variable binding))
+                   (val (binding-value binding)))
+               (let ((binding-in-f2 (binding-in-frame var f2)))
+                 (cond ((not binding-in-f2) false)
+                       ((not (equal? val (binding-value binding-in-f2))) false)
+                       (equal-frame? (cdr f1)
+                                     (delete-binding var f2)))))))))
 
-                   ...)
-
-
-                  (else false))))))
   (define (accumulate-unique-frames frame frames)
     (cond ((stream-null? frames)
            (cons-stream frame frames))
-          ((equal? (stream-car frames) frame)
+          ((equal-frame? (stream-car frames) frame)
            (stream-cdr frames))
           (else
-           (stream-cons (stream-car frames)
+           (cons-stream (stream-car frames)
                         (accumulate-unique-frames frame (stream-cdr frames))))))
   (stream-flatmap
    (lambda (frame)
+     (display frame)
+     (newline)
      (set! unique-frames (accumulate-unique-frames frame unique-frames)))
-   (qeval pattern frame-stream))
+
+   (qeval (car operands) frame-stream))
   unique-frames)
 
 
 (put 'unique 'qeval unique-asserted)
 
+(define sample "
+
+(assert! (assert! (address (Bitdiddle Ben) (Slumerville (Ridge Road) 10))))
+(assert! (job (Bitdiddle Ben) (computer wizard)))
+(assert! (salary (Bitdiddle Ben) 60000))
+(assert! (address (Hacker Alyssa P) (Cambridge (Mass Ave) 78)))
+(assert! (job (Hacker Alyssa P) (computer programmer)))
+(assert! (salary (Hacker Alyssa P) 40000))
+(assert! (supervisor (Hacker Alyssa P) (Bitdiddle Ben)))
+(assert! (address (Fect Cy D) (Cambridge (Ames Street) 3)))
+(assert! (job (Fect Cy D) (computer programmer)))
+(assert! (salary (Fect Cy D) 35000))
+(assert! (supervisor (Fect Cy D) (Bitdiddle Ben)))
+(assert! (address (Tweakit Lem E) (Boston (Bay State Road) 22)))
+(assert! (job (Tweakit Lem E) (computer technician)))
+(assert! (salary (Tweakit Lem E) 25000))
+(assert! (supervisor (Tweakit Lem E) (Bitdiddle Ben)))
+(assert! (address (Reasoner Louis) (Slumerville (Pine Tree Road) 80)))
+(assert! (job (Reasoner Louis) (computer programmer trainee)))
+(assert! (salary (Reasoner Louis) 30000))
+(assert! (supervisor (Reasoner Louis) (Hacker Alyssa P)))
+(assert! (supervisor (Bitdiddle Ben) (Warbucks Oliver)))
+(assert! (address (Warbucks Oliver) (Swellesley (Top Heap Road))))
+(assert! (job (Warbucks Oliver) (administration big wheel)))
+(assert! (salary (Warbucks Oliver) 150000))
+(assert! (address (Scrooge Eben) (Weston (Shady Lane) 10)))
+(assert! (job (Scrooge Eben) (accounting chief accountant)))
+(assert! (salary (Scrooge Eben) 75000))
+(assert! (supervisor (Scrooge Eben) (Warbucks Oliver)))
+(assert! (address (Cratchet Robert) (Allston (N Harvard Street) 16)))
+(assert! (job (Cratchet Robert) (accounting scrivener)))
+(assert! (salary (Cratchet Robert) 18000))
+(assert! (supervisor (Cratchet Robert) (Scrooge Eben)))
+(assert! (address (Aull DeWitt) (Slumerville (Onion Square) 5)))
+(assert! (job (Aull DeWitt) (administration secretary)))
+(assert! (salary (Aull DeWitt) 25000))
+(assert! (supervisor (Aull DeWitt) (Warbucks Oliver)))
+(assert! (can-do-job (computer wizard) (computer programmer)))
+(assert! (can-do-job (computer wizard) (computer technician)))
+(assert! (can-do-job (computer programmer)
+                     (computer programmer trainee)))
+(assert! (can-do-job (administration secretary)
+                     (administration big wheel)))
+
+(unique (job ?x (computer wizard)))
+(unique (job ?x (computer programmer)))
+
+")
 
 ;; to be continued
