14662e21fb274ca14bd38eb7e2d66634d8dc687d
Kazuya NUMATA <numata@gmail.com>
2016-10-06 18:03:03 +0900

    (WIP) Implement ch5.1.4's examples

--- ch5/examples/5-1-4.scm
+++ ch5/examples/5-1-4.scm
@@ -10,5 +10,33 @@
 ;; gcd reduces the original computation to the new gcd computation,
 ;; but new factorial computation's result is not the answer of the original gcd. ( N * (N - 1)! )
 
+;; In the subproblem, the contents of the registeres will be different than they were in the main problem, then store the contents of the registers which are needed after the subproblem is solved in order to restore to continue the suspended computation.
+
+;; Use 'stack' which enables 'last-in, first-out' to save the contents of the registers.
+
+;; To satisfy this, we add two instructions, 'save' and 'restore'.
+;; So as to 'continue' register.
+
+(controller
+ (assign continue (label (fact-done)))
+ fact-loop
+ (test (op =) (reg n) (const 1))
+ (branch (label base-case))
+ (save continue)
+ (save n)
+ (assign n (op -) (reg n) (const 1))
+ (assign continue (label after-fact))
+ (goto (label fact-loop))
+ after-fact
+ (restore n)
+ (restore continue)
+ (assign val (op *) (reg n) (reg val))
+ (goto (reg continue))
+ base-case
+ (assign val (const 1))
+ (goto (reg continue))
+ fact-done)
+
+;; A double recursion
 
 ;; to be continued
